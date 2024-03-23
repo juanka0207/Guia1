5414708e43918d1f3593d21fd468a5a0501fca39
juanka0207 <juan.gaviria@unicolombo.edu.co>
2024-03-22 22:44:34 -0500

    mi primer commit

---
+++ ejer1.js
@@ -0,0 +1,16 @@
+alert("BIENVENIDA");
+
+let nombre = luna;
+let edad = 25;
+let numeroDeVenta = 50;
+let saldoDisponible = 1000;
+let error = "¡Error! completa todo los campos";
+alert(error);
+
+let nombres = prompt ("ingrese su nombre de");
+let edads = prompt("Digite su edad");
+
+let eda = prompt ("digite su edad");
+if (eda >= 18){
+    alert("puedes obtener tu licencia de conducior");
+}
